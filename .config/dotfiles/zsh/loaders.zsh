3d3e6cff69be64dc555c7bef9ed4738df4747c8b
Fernando <fwfurtado@gmail.com>
2020-11-23 09:38:16 -0300

    Update wal configuration

--- .config/dotfiles/zsh/loaders.zsh
+++ .config/dotfiles/zsh/loaders.zsh
@@ -21,5 +21,5 @@ source ~/.asdf/plugins/java/set-java-home.zsh
 poetry completions zsh > "$ZINIT_HOME/completions/_poetry" && compinit 
 
 [ -f $HOME/.cache/wal/colors.Xresources ] && ln -sf $HOME/.cache/wal/colors.Xresources $HOME/.Xresources 
-[ -f $HOME/.config/nitrogen/bg-saved.cfg ] && wal -a 8 -n -e -i $(cat $HOME/.config/nitrogen/bg-saved.cfg | grep -m 1 'file=' | cut -f 2 -d '=') > /dev/null
+[ -f $HOME/.config/wal/launcher.sh ] && source $HOME/.config/wal/launcher.sh 
 
---
+++ .config/wal/launcher.sh
@@ -0,0 +1,12 @@
+current_desktop=$(echo $XDG_CURRENT_DESKTOP | tr '[:upper:]' '[:lower:]') 
+
+case $current_desktop in 
+  deepin)
+    current_background=$(grep '=' $HOME/.config/deepinwmrc | sed -n '1 p' | cut -d '=' -f 2 | sed 's/file:\/\///')
+    ;;
+  *)
+    echo "Sorry I don't know what is your current desktop"
+    ;;
+esac 
+
+wal -a 8 -n -e -i "$current_background" > /dev/null
