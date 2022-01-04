c707dfb1b3f2b7220253cdc2a6d9cb9a2b558561
Fernando <fwfurtado@gmail.com>
2022-01-04 12:58:35 -0300

    Remove wall for fish

--- .config/dotfiles/fish/loaders.fish
+++ .config/dotfiles/fish/loaders.fish
@@ -7,10 +7,10 @@ test -e $HOME/.asdf/plugins/java/set-java-home.fish; and source $HOME/.asdf/plug
 # Set dotnet-core to current asdf java
 test -e $HOME/.asdf/plugins/dotnet-core/set-dotnet-home.fish; and source $HOME/.asdf/plugins/dotnet-core/set-dotnet-home.fish
 
-test -e $HOME/.config/wal/launcher.fish; and source $HOME/.config/wal/launcher.fish
-test -e $HOME/.cache/wal/colors-tty.sh; and sh $HOME/.cache/wal/colors-tty.sh >> /dev/null 
-test -e $HOME/.cache/wal/colors.fish; and source $HOME/.cache/wal/colors.fish 
-test -e $HOME/.cache/wal/colors.Xresources; and ln -sf $HOME/.cache/wal/colors.Xresources $HOME/.Xresources
+#test -e $HOME/.config/wal/launcher.fish; and source $HOME/.config/wal/launcher.fish
+#test -e $HOME/.cache/wal/colors-tty.sh; and sh $HOME/.cache/wal/colors-tty.sh >> /dev/null 
+#test -e $HOME/.cache/wal/colors.fish; and source $HOME/.cache/wal/colors.fish 
+#test -e $HOME/.cache/wal/colors.Xresources; and ln -sf $HOME/.cache/wal/colors.Xresources $HOME/.Xresources
 
 # bass source $HOME/.nix-profile/etc/profile.d/nix.sh
 
